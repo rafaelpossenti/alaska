f3f00a6978fe26d3c2eca7260d79645c8c67bc9e
Rafael Possenti <rafael.possenti@wssim.com.br>
2020-02-25 10:56:38 -0300

    (Issue #1) Created the project structure

---
+++ src/main/java/com/possenti/alaska/AlaskaApplication.java
@@ -0,0 +1,13 @@
+package com.possenti.alaska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaskaApplication.class, args);
+	}
+
+}
